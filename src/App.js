2f0804e8b29f735b4daca473ad7bf47de35ee4e0
Felipe Silva <felipe.dacs@gmail.com>
2021-06-03 02:32:28 -0300

    :tv: add aula 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import {GlobalStyle} from './Components/GlobalStyle.js'
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Cabecalho />
       <Container />
     </>
